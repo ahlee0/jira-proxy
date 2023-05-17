a07090f811ae131e144079342358542c08ff7cc9
Ali Shah <ali.shah@digital.hmrc.gov.uk>
2023-05-17 12:58:13 +0100

    using http-proxy-middleware

--- app.js
+++ app.js
@@ -1,31 +1,27 @@
 const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 const axios = require("axios");
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send("Welcome to the JIRA proxy server!"));
 
-app.get("/jira/:filterId", async (req, res) => {
-  const filterId = req.params.filterId;
-  const jiraApiUrl = `https://jira.tools.tax.service.gov.uk/rest/api/2/filter/${filterId}`;
-
-  try {
-    const response = await axios.get(jiraApiUrl, {
-      headers: {
-        // Add any required headers here, such as an API key or authentication
-        'Authorization': req.headers.authorization,
-      },
-    });
-
+const jiraProxy = createProxyMiddleware('/jira', {
+  target: 'https://jira.tools.tax.service.gov.uk',
+  pathRewrite: { '^/jira': '/rest/api/2/filter' },
+  changeOrigin: true,
+  onProxyReq(proxyReq, req, res) {
+    // Add any required headers here, such as an API key or authentication
+    proxyReq.setHeader('Authorization', req.headers.authorization);
+  },
+  onProxyRes(proxyRes, req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
-
-    res.status(200).json(response.data);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
+  },
 });
+
+app.use(jiraProxy);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
